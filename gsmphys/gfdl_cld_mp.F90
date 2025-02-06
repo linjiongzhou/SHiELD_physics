bdd02b96865213130c3a09965016e6da8abd6fbf
Linjiong Zhou <Linjiong.Zhou@noaa.gov>
2025-02-06 11:43:09 -0500

    Share mte with other routines.

--- gsmphys/gfdl_cld_mp.F90
+++ gsmphys/gfdl_cld_mp.F90
@@ -81,7 +81,7 @@ module gfdl_cld_mp_mod
     public :: cld_sat_adj, cld_eff_rad, rad_ref
     public :: qs_init, wqs, mqs, mqs3d
     public :: c_liq, c_ice, rhow, wet_bulb
-    public :: cv_air, cv_vap, mtetw
+    public :: cv_air, cv_vap, mtetw, mte
     public :: hlv, hlf, tice
 
     ! -----------------------------------------------------------------------
